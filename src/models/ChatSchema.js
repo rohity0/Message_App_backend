4c3f83cb32b4c7ecf098521d7f8b3eaa8b95592d
Rohit Yadav <101393562+rohity0@users.noreply.github.com>
2024-04-30 01:15:09 +0530

    Update ChatSchema.js

--- src/models/ChatSchema.js
+++ src/models/ChatSchema.js
@@ -5,7 +5,7 @@ const chatSchema = new Schema(
   {
     chatName: { type: String, trim: true },
     isGroupChat: { type: Boolean, default: false },
-    users: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    users: [{ type: Schema.Types.ObjectId, ref: "User", unique: true  }],
     latestMessage: { type: Schema.Types.ObjectId, ref: "Message" },
   },
   { timestamps: true }
